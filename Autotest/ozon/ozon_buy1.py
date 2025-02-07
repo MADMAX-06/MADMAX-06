2666ec2422f3a13bf2fc290f2bf0829d9e4605c4
MAXIM RYABOV <maxim_ryabov@rambler.ru>
2025-02-08 00:42:24 +0300

    Внесены изменения

--- Autotest/ozon/ozon_buy1.py
+++ Autotest/ozon/ozon_buy1.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium_stealth import stealth
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
+
+
 
 options = webdriver.ChromeOptions()
 options.add_argument("start-maximized")
@@ -30,17 +30,47 @@ time.sleep(1)
 input1.send_keys("Книга Python")
 
 time.sleep(1)
-button1 = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/header/div[1]/div/div[2]/div/div[2]/form/button")
+button1 = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
 button1.click()
 time.sleep(1)
 element = driver.find_element(By.XPATH, "//*[text()='Программируем на Python | Доусон Майкл']")
 element.click()
-time.sleep(3)
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "jz0_27 b2121-a0 b2121-b2 b2121-a4")))
-book1 = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div[6]/div/div[1]/div[1]/div/div[1]/div/div[1]/div[2]/div/button/div[2]")
-book1.click()
 
+windows = driver.window_handles
+driver.switch_to.window(windows[1])
+
+get_name_of_book = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div[3]/div[3]/div[1]/div[1]/div[2]/div/div/div/div[1]/h1")
+name_of_book = get_name_of_book.text
+
+find_price = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div[3]/div[3]/div[2]/div/div/div[1]/div[2]/div/div[1]/div/div/div[1]/div[1]/button/span/div/div[1]/div/div")
+get_price = find_price.text
 
 time.sleep(2)
+book1 = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div[3]/div[3]/div[2]/div/div/div[1]/div[2]/div[1]/div[4]/div/div/div[1]/div/div/div/div[1]/button")
+book1.click()
+
+time.sleep(2)
+basket = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/header/div[1]/div/div[3]/div[4]")
+basket.click()
+
+windows_2 = driver.window_handles
+driver.switch_to.window(windows_2[2])
+
+time.sleep(2)
+get_name_of_book_in_basket = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div[2]/div/div[2]/div[4]/div[1]/div/div/div[2]/div/div/div[2]/div/div/div/div/div[3]/div/div[1]/div/div")
+name_of_book_in_basket = get_name_of_book_in_basket.text
+
+get_price_in_basket = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div[2]/div/div[2]/div[4]/div[1]/div/div/div[2]/div/div/div[2]/div/div/div/div/div[4]/div/div[1]/div/div/span[1]")
+price_in_basket = get_price_in_basket.text
+
+#Проверяет соответствие товара в корзине
+def test_check_name_of_book():
+        assert name_of_book == name_of_book_in_basket, f"Ожидаемый результат: name_of_book. Фактический результат: name_of_book_in_basket"
+
+#Проверяет соответствие цены в корзине
+def test_check_price():
+        assert get_price == price_in_basket, f"Ожидаемый результат: get_price. Фактический результат: price_in_basket"
+
+time.sleep(5)
 # закрываем браузер после всех манипуляций
 driver.quit()
