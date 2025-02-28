a8b84c0610d49f7b882a417baea0a7e64b8da664
MAXIM RYABOV <maxim_ryabov@rambler.ru>
2025-02-28 23:59:21 +0300

    Внесены изменения

--- Autotest/t2/test_t2_check_tarif.py
+++ Autotest/t2/test_t2_check_tarif.py
@@ -11,3 +11,8 @@ class TestCheckTarif(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def test_check_tarif_hvatit(self):
+        test_passed = "ХВАТИТ!"
+        self.browser.get(self.link)
+        element = self.browser.find_element(By.XPATH, "/html/body/div[2]/div/div[1]/div/div/div/div/div[2]/div[2]/section/section/div/div/div/div[1]/a/div/div[1]/div[1]/h3")
+        
